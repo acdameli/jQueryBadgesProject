1343bbb0efa0b6af978e174dc6649d422ac4a20b
Craig D'Amelio <acdameli@users.noreply.github.com>
2016-12-24 15:36:36 -0800

    make it work

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,17 @@ $(function() {
 
   // your code will go here
 
+  $.ajax(
+      'https://www.codeschool.com/users/acdameli.json',
+      {
+          'dataType': 'jsonp',
+          'success': function(a,b,c) {
+              var divs = '';
+              $.each(a.courses.completed, function() {
+                  divs += '<div class="course"><img src="' + this.badge + '" /><h3>' + this.title + '</h3><a class="btn btn-primary" target="_blank" href="' + this.url + '">See Course</a><img src="' + this.badge + '" /></div>'
+              });
+              $('#badges').append(divs);
+          }
+      }
+  );
 });
